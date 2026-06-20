7d500a069b97c98952c61e554eb3cd6743e1c131
.warasugi <76039679+warasugitewara@users.noreply.github.com>
2026-06-20 09:04:45 +0900

    feat: Task 7 - ElevatorPlugin メインクラスの実装
    - onEnable()で全コンポーネント（ConfigManager, ElevatorListener, ElevatorCommand）を初期化
    - サーバー起動時に設定読込・リスナー登録・コマンド登録を実行
    - ビルド確認済み

---
+++ src/main/java/com/warasugitewara/elevator/ElevatorPlugin.java
@@ -0,0 +1,29 @@
+package com.warasugitewara.elevator;
+
+import com.warasugitewara.elevator.command.ElevatorCommand;
+import com.warasugitewara.elevator.config.ConfigManager;
+import com.warasugitewara.elevator.listener.ElevatorListener;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElevatorPlugin extends JavaPlugin {
+
+	private ConfigManager configManager;
+
+	@Override
+	public void onEnable() {
+		configManager = new ConfigManager(this);
+		configManager.load();
+
+		getServer().getPluginManager().registerEvents(new ElevatorListener(configManager), this);
+
+		ElevatorCommand command = new ElevatorCommand(configManager);
+		PluginCommand pluginCommand = getCommand("elevator");
+		if (pluginCommand != null) {
+			pluginCommand.setExecutor(command);
+			pluginCommand.setTabCompleter(command);
+		}
+
+		getLogger().info("IronElevators enabled.");
+	}
+}
